07949ae500364e53dd678777bfbdf4615c365d22
Eduardo Cruz <eduardo.cruz@ibm.com>
2023-07-02 00:29:32 -0600

    update

--- client/src/winner_screen/view.tsx
+++ client/src/winner_screen/view.tsx
@@ -12,10 +12,21 @@ const VictoryScreen = (
     { winPlayer, onClickReplay } : VictoryScreenProps) => {
     return(
         <article className="VictoryScreen">
-            <div className="VictoryScreen-box dark:bg-gradient-to-r from-slate-800 to-slate-900 ">
-                <h2 className="VictoryScreen-title mt-6">Winner:</h2>
-                <p className="VictoryScreen-player">{winPlayer}</p>
-                <button onClick={onClickReplay} className="VictoryScreen-replay bg-gradient-to-r from-cyan-500 to-blue-500 ">Replay</button>
+            <div className="
+                VictoryScreen-box 
+                dark:bg-gradient-to-r from-slate-800 to-slate-900 ">
+                <h2 
+                    className="
+                        VictoryScreen-title 
+                        mt-6 text-2xl lg:text-5xl">{winPlayer === Player.tie ? '    ': 'Winner:'}</h2>
+                <p className="VictoryScreen-player text-5xl">{winPlayer}</p>
+                <button 
+                    onClick={onClickReplay} 
+                    className="
+                        VictoryScreen-replay 
+                        bg-gradient-to-r from-cyan-500 to-blue-500
+                        min-w-[100px]
+                        text-lg">Replay</button>
             </div>
         </article>
     )
